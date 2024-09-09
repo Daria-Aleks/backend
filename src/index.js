13c38e1a15db1e7d13741e6d75624fe6bdf7fa79
Daria <daria.aleks1@yandex.ru>
2024-09-09 12:07:49 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,59 +1,52 @@
-const http = require('http')
-const getUsers = require('./modules/users.js')
+const http = require('http');
+const getUsers = require('./modules/users.js');
 
 const port = 3003;
-const hostname ="http://127.0.0.1";
+const hostname = "http://127.0.0.1";
 
 const server = http.createServer((request, response) => {
- 
+
   const ipAddress = "http://127.0.0.1";
   const url = new URL(request.url, ipAddress);
-  const userName = url.searchParams.get("hello");
 
-  if (userName) {
+  const isUserParam = url.searchParams.has("users");
+  const isHelloParam = url.searchParams.has("hello");
+
+  if (isHelloParam) {
+    const userName = url.searchParams.get("hello");
+    if (userName) {
+      response.statusCode = 200;
+      response.statusMessage = "ok";
+      response.setHeader("Content-Type", "text/plain");
+      response.write(`Hello, my friend ${userName}`);
+      response.end();
+    } else {
+      response.statusCode = 400;
+      response.statusMessage = "Bad Request";
+      response.setHeader("Content-Type", "text/plain");
+      response.write(`Enter a name`);
+      response.end();
+    }
+    return;
+  }
+
+  if (isUserParam) {
     response.statusCode = 200;
-    response.statusMessage = "ok";
-    response.setHeader("Content-Type", "text/plain");
-    response.write(`Hello, my friend ${userName}`);
+    response.statusMessage = "OK";
+    response.setHeader("Content-Type", "application/json");
+    response.write(getUsers());
     response.end();
     return;
-  };
+  }
 
- switch (request.url) {
-    case "/users":
-      response.statusCode = 200;
-      response.statusMessage = "OK";
-      response.setHeader("Content-Type", "application/json");
-      response.write(getUsers());
-      response.end();
-      break;
-
-case "/?hello":
-  response.statusCode = 400;
-  response.statusMessage = "Bad Request";
+  response.statusCode = 404;
+  response.statusMessage = "Not Found";
   response.setHeader("Content-Type", "text/plain");
-  response.write(`Enter a name`);
+  response.write("Not Found");
   response.end();
-  break;
 
-case "/":
-  response.statusCode = 200;
-  response.statusMessage = "OK";
-  response.setHeader("Content-Type", "text/plain");
-  response.write(`Hello world`);
-  response.end();
-  break;
-
-default:
-  response.statusCode = 500;
-  response.statusMessage = "Internal Server Error";
-  response.setHeader("Content-Type", "text/plain");
-  response.write("wrong");
-  response.end();
-  break;
-}
-})
+});
 
 server.listen(port, () => {
-  console.log(`Сервер запущен по адресу ${hostname}:${port}/`);
+  console.log(`Server running at ${hostname}:${port}/`);
 });
